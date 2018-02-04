a29cbe6a1cc5f207af413d69f4150f614ea69c3a
claudito <1107000044@upci.edu.pe>
2018-02-04 18:03:53 -0500

    consultas mediante procedimientos almacenados

--- modelo/Reporte.php
+++ modelo/Reporte.php
@@ -9,10 +9,11 @@ function top_compras($top=10)
 try {
 
 $conexion =  Conexion::get_conexion();
-$query =  "SELECT c.ruc,c.razon_social,SUM(d.cantidad) as cantidad  FROM compras_cab c
+/*$query =  "SELECT c.ruc,c.razon_social,SUM(d.cantidad) as cantidad  FROM compras_cab c
 INNER JOIN compras_det d ON c.numero=d.numero
 GROUP by c.ruc
-ORDER BY SUM(d.cantidad)  DESC limit ".$top."";
+ORDER BY SUM(d.cantidad)  DESC limit ".$top."";*/
+$query  = "CALL top_compras(".$top.")";
 $statement =  $conexion->prepare($query);
 //$statement->bindParam(':top',$top);
 $statement->execute();
